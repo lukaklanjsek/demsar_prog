23ecebfc86dd8c4ca3eb39f0effb18377ddf7bfc
lukaklanjsek <luka.klanjsek@gmail.com>
2025-03-18 14:23:33 +0100

    dictionary added
    hi this time i am starting the program with adding a dictionary of letters and their accompanying number value

--- play_around.py
+++ play_around.py
@@ -1,3 +1,4 @@
+'''
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
@@ -11,3 +12,25 @@ print(z)
 
 for e in z:  #why 'e' works???
     print(e)
+'''
+import sys
+print(sys.version)
+# sys.argv = []
+print(sys.argv)
+
+the_message = list(sys.stdin)
+
+print(the_message, type(the_message))
+
+
+
+for line in sys.stdin:
+    if 'q' == line.rstrip():
+        break
+    print(f'Input : {line}')
+
+print("Exit")
+print(sys.argv)
+
+txt = "The best things in life are free!"
+print("free" in txt)
