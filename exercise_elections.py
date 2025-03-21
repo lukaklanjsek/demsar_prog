7271bcc23ac76fbb6e18a970c60505298e5d350a
lukaklanjsek <luka.klanjsek@gmail.com>
2025-03-21 23:02:46 +0100

    failed list comprehention
    I admit defeat in list comprehension exercise, it simply did not work for me when I wanted to add more statements.

---
+++ exercise_elections.py
@@ -0,0 +1,57 @@
+"""
+It is election season! The candidates are numbered from  1 to StKandidatov, which is smaller or same than MaxStKandidatov.
+Assemble a subprogram that would draw a histogram of results.
+
+shape and form of expected subprogram:
+
+const MaxStVolilcev = ...; MaxStKandidatov = ...;
+type GlasoviT = array [1..MaxStKandidatov] of integer;
+
+procedure Histogram(StKandidatov, StVolilvec: integer; Glasovi: GlasoviT);
+begin
+ ...
+end;
+"""
+
+# begin:
+
+max_st_volilcev = int(input("Najvecje stevilo volilcev je: "))
+
+max_st_kandidatov = int(input("Najvecje stevilo kandidatov je: "))
+st_volilcev = 0
+st_kandidatov = 0
+
+
+
+while True:
+    st_kandidatov = int(input("Koliko je dejanskih kandidatov: " ))
+    if 0 < st_kandidatov <= max_st_kandidatov:
+        break
+    else:
+        print(f"Vnos dejanskega števila kandidatov, med 1 in {max_st_kandidatov}")
+
+while True:
+    st_volilcev = int(input("Koliko vas dejansko voli: "))
+    if 0 < st_volilcev <= max_st_volilcev:
+        break
+    else:
+        print(f"Prosimo vnesite število volilcev, ki je med 1 in {max_st_volilcev}")
+
+glasovi_T = []
+i = 0
+for i in range(0, st_volilcev):
+    while True:
+        try:
+
+            glas = int(input(f"Vnesi glasove enega na enkrat: {i+1}.:"))
+            if 0 < glas <= max_st_kandidatov:
+               glasovi_T.append(glas)
+               break
+            else:
+                print(f"Iščemo številko med 1 in {max_st_kandidatov}")
+        except ValueError:
+            print("Prosim vnesite številko.")
+
+print("list of glasovi_T: ", glasovi_T)    # test print to see where we are
+
+
