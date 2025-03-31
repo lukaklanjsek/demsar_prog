9aac4224dba815dee8a9e93645dd61c50d5eb535
lukaklanjsek <luka.klanjsek@gmail.com>
2025-03-31 12:26:14 +0200

    new exercise
    this is a new exercise "recalculations" i have only began working on it.

---
+++ recalculations_exercise.py
@@ -0,0 +1,23 @@
+"""
+Write a program, that would use standard input of ten rows with seven numbers, followed by a row of ten numbers, that would tell order sufficiency.
+input values order: lige, milje, furlongi, jardi, čevlji, dlani, palci
+by the relations between values:
+    1 liga = 3 milje,
+    1 milja = 8 furlongov,
+    1 furlong = 220 jardov,
+    1 jard = 3 čevlji,
+    1 čevelj = 3 dlani,
+    1 dlan = 4 palci;
+Use the biggest units possible.
+
+example: 5000 jardov = 0 2 6 160 0 0 0.
+"""
+
+#import sys
+
+#user_imput = sys.argv[1:]
+
+print("Hello and welcome the recalculations exercise. Please enter your orders:")
+vnos1 = [input("1: " )]
+
+print(vnos1)    # test print
